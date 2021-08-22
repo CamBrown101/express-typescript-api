fbafbd0befde95550439569135706e2c97981964
CamBrown101 <cameron-brown@live.com>
2021-08-22 15:42:31 -0700

    Connect to DB

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import express from "express"
+import mongoose from 'mongoose'
 const app = express();
+
+//Connect to DB
+const mongooseConnect: string = 'mongodb+srv://cam:test123@cluster0.ber4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongooseConnect, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to DB'))
 
 //Import Routes
 const authRoute = require('./routes/auth');

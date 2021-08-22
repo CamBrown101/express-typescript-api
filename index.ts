94cb418f72edb4f76624075bda60cb269e60f8bd
CamBrown101 <cameron-brown@live.com>
2021-08-22 15:49:30 -0700

    test

--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
-import express from "express"
-import mongoose from 'mongoose'
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
 
 //Connect to DB
-const mongooseConnect: string = 'mongodb+srv://cam:test123@cluster0.ber4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const mongooseConnect: string =
+  "mongodb+srv://cam:test3@cluster0.ber4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(mongooseConnect, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to DB'))
+mongoose.connect(
+  mongooseConnect,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to DB")
+);
 
 //Import Routes
-const authRoute = require('./routes/auth');
-
+import authRoute from "./routes/auth";
 
 //Route Middlewares
-app.use('/api/user', authRoute)
-
+app.use("/api/user", authRoute);
 
 const port = process.env.POrt || 3000;
 app.listen(port, () => console.log(`App listening on PORT ${port}`));

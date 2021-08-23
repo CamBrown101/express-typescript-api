06f7a58cc60e97133043d9d2cf3ad6114d60c96f
CamBrown101 <cameron-brown@live.com>
2021-08-22 17:49:17 -0700

    Add duplication check

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,28 +1,28 @@
 import { Request, Response } from "express";
 import express from "express";
 import User from "../model/User";
+import { registerValidation } from "../validation";
 
 const router = express.Router();
 
-//Validation
-import Joi from "@hapi/joi";
-
-const schema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
-
 router.post("/register", async (req: Request, res: Response) => {
   //Validate data
-  const { error } = schema.validate(req.body);
-  if (error) res.send(error.details[0].message);
+  const { error } = registerValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  //Check if user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
 
+  //Create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
+
+  //Save user
   try {
     const savedUser = await user.save();
     res.send(savedUser);
